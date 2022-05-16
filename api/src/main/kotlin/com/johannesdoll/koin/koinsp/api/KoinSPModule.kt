23cca24a8f84f5ef1ea4082757e80c322eaedde5
Johannes Doll <git@johannesdoll.com>
2022-05-17 00:46:16 +0200

    Add KoinSP annotation

---
+++ api/src/main/kotlin/com/johannesdoll/koin/koinsp/api/KoinSPModule.kt
@@ -0,0 +1,5 @@
+package com.johannesdoll.koin.koinsp.api
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+annotation class KoinSPModule()
